a74834502eeaad76a74f4d4bb4089ee675ce3e77
lyc <893608341@qq.com>
2019-06-26 18:09:47 +0800

    fix

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
+<<<<<<< Updated upstream
     <h1>This is an dddddabddddfdfdout page</h1>hjh
+=======
+    <h1>This is an dddddabout page</h1>hjh   策划师
+>>>>>>> Stashed changes
   </div>
 </template>
